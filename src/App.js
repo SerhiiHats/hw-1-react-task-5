ea7d72c9caff3375d1101ba5d9bf39bb67ed9153
Serhi Hats <gatsserv@gmail.com>
2023-04-26 01:29:40 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Завдання 5</h3>
+        <p>
+          Створіть застосунок за допомогою Create React App і виведіть на сторінку текст «Hello world»
+        </p>
+      </header>
+      <main>
+        <p>Hello World!!!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
